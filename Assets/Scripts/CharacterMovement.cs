c197b83056be10c87dfe15df2348f25f108e41ad
Mehmet Emin Çoban <mehmetemincoban2110@gmail.com>
2025-12-10 00:58:31 +0300

    Karakter hareket sistemi eklendi. Animasyonlar uygun şekilde çalıştırıldı.
    Ufak bir yön bugı var. Düzeltilecek.

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    Animator anim;
+    Vector2 movement;
+
+    PlayerControls controls;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+    }
+
+    void OnEnable() => controls.Player.Enable();
+    void OnDisable() => controls.Player.Disable();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement = controls.Player.Move.ReadValue<Vector2>().normalized;
+
+        anim.SetFloat("moveX", movement.x);
+        anim.SetFloat("moveY", movement.y);
+        anim.SetFloat("speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
