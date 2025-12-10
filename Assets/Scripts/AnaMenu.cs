e28b0f136419db889ed988bd62e7bacc4a93f849
ArdaOktar67 <oktararda@hotmail.com>
2025-12-10 14:19:54 +0300

    Settings Code

--- Assets/Scripts/AnaMenu.cs
+++ Assets/Scripts/AnaMenu.cs
@@ -1,16 +1,36 @@
+using System;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class AnaMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject SettingsPanel;
+    public GameObject CreditsPanel;
+
+    public void PLAY()
     {
-        
+        SceneManager.LoadScene("Game");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenSetPanel()
     {
-        
+        SettingsPanel.SetActive(true);
     }
+
+    public void CloseSetPanel()
+    {
+        SettingsPanel.SetActive(false);
+    }
+
+    public void OpenCreditsPanel()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CloseCreditsPanel()
+    {
+        CreditsPanel.SetActive(false);
+    }
+
+
 }
+   
